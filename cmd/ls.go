6aac9273abdbca4219b122324c513af13e8b536e
Marco Ferragina <marco.ferragina@gmail.com>
2022-10-23 18:25:01 +0200

    list tables

--- cmd/ls.go
+++ cmd/ls.go
@@ -76,13 +76,43 @@ func listSchemas(connString string, db string) {
 	defer rows.Close()
 
 	for rows.Next() {
-		var datname string
-		err = rows.Scan(&datname)
+		var schema string
+		err = rows.Scan(&schema)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 			os.Exit(1)
 		}
-		fmt.Println(datname)
+		fmt.Println(schema)
+	}
+}
+
+func listTables(connString string, db string, schema string) {
+	conn, err := pool.GetFromConf(connString, db)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	query := fmt.Sprintf(`
+		SELECT table_name 
+		FROM information_schema.tables 
+		WHERE table_schema = '%s'`, schema)
+	rows, err := conn.Query(context.Background(), query)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var tableName string
+		err = rows.Scan(&tableName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(tableName)
 	}
 }
 
@@ -106,6 +136,11 @@ var recordCmd = &cobra.Command{
 			conn := parts[0]
 			database := parts[1]
 			listSchemas(conn, database)
+		case 3:
+			conn := parts[0]
+			database := parts[1]
+			schema := parts[2]
+			listTables(conn, database, schema)
 		}
 	},
 }
