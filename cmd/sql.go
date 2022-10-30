5987e4c97dd9100ecda7e3cc9e6af298f67e729f
Marco Ferragina <marco.ferragina@gmail.com>
2022-10-30 09:15:36 +0100

    added sql editor

--- cmd/sql.go
+++ cmd/sql.go
@@ -2,12 +2,16 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/ferama/pg/pkg/autocomplete"
 	"github.com/ferama/pg/pkg/db"
 	"github.com/ferama/pg/pkg/utils"
 	"github.com/spf13/cobra"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func init() {
@@ -23,21 +27,88 @@ func sqlExecute(connString, dbName, schema, query string) {
 	db.PrintQueryResults(items, fields)
 }
 
+type model struct {
+	textarea textarea.Model
+	err      error
+}
+
+func newModel() model {
+	ti := textarea.New()
+	ti.Placeholder = "SELECT..."
+	ti.SetWidth(60)
+	ti.Focus()
+
+	return model{
+		textarea: ti,
+		err:      nil,
+	}
+}
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			m.textarea.Reset()
+			return m, tea.Quit
+		case tea.KeyCtrlR:
+			return m, tea.Quit
+		default:
+			if !m.textarea.Focused() {
+				cmd = m.textarea.Focus()
+				cmds = append(cmds, cmd)
+			}
+		}
+
+	// We handle errors just like any other message
+	case error:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textarea, cmd = m.textarea.Update(msg)
+	cmds = append(cmds, cmd)
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"\n%s\n\n%s",
+		m.textarea.View(),
+		"(ctrl+r to run. ESC to cancel)",
+	) + "\n\n"
+}
+
 var sqlCmd = &cobra.Command{
 	Use:               "sql",
-	Args:              cobra.MinimumNArgs(2),
+	Args:              cobra.MinimumNArgs(1),
 	Short:             "Run sql query",
 	ValidArgsFunction: autocomplete.Path(3),
 	Run: func(cmd *cobra.Command, args []string) {
 		path := utils.ParsePath(args[0], false)
 
 		if path.SchemaName != "" {
-			sqlExecute(
-				path.ConfigConnection,
-				path.DatabaseName,
-				path.SchemaName,
-				args[1],
-			)
+			model := newModel()
+			p := tea.NewProgram(model)
+
+			if err := p.Start(); err != nil {
+				log.Fatal(err)
+			}
+			query := model.textarea.Value()
+			if query != "" {
+				sqlExecute(
+					path.ConfigConnection,
+					path.DatabaseName,
+					path.SchemaName,
+					query,
+				)
+			}
 		} else {
 			fmt.Fprintf(os.Stderr, "schema '%s' not found", path.SchemaName)
 			os.Exit(1)
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/ferama/pg
 go 1.19
 
 require (
+	github.com/charmbracelet/bubbles v0.14.0
+	github.com/charmbracelet/bubbletea v0.22.1
 	github.com/jackc/pgx/v5 v5.0.0
 	github.com/jedib0t/go-pretty/v6 v6.4.0
 	github.com/spf13/cobra v1.6.0
@@ -10,15 +12,25 @@ require (
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/charmbracelet/lipgloss v0.5.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/puddle/v2 v2.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
@@ -29,7 +41,8 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
