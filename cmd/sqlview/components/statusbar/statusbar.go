2179904bbd91ff8ba16dbe4d3074731f443ef04c
Marco Ferragina <marco.ferragina@gmail.com>
2022-11-05 21:03:55 +0100

    better status bar

--- cmd/sqlview/components/statusbar/statusbar.go
+++ cmd/sqlview/components/statusbar/statusbar.go
@@ -1,25 +1,38 @@
 package statusbar
 
 import (
+	"fmt"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/ferama/pg/pkg/conf"
+	"github.com/ferama/pg/pkg/utils"
 )
 
 var (
-	barStyle = lipgloss.NewStyle().
-		Background(lipgloss.Color("#cccccc")).
-		Foreground(lipgloss.Color("#000000")).
-		Align(lipgloss.Right).
-		Border(lipgloss.ThickBorder(), false, true).
-		BorderForeground(lipgloss.Color(conf.ColorBlur))
+	shortcutStyle = lipgloss.NewStyle().
+			Background(lipgloss.Color("#cccccc")).
+			Foreground(lipgloss.Color("#000000")).
+			Align(lipgloss.Right).
+			Border(lipgloss.ThickBorder(), false, true, false, false).
+			BorderForeground(lipgloss.Color(conf.ColorBlur))
+
+	infoStyle = lipgloss.NewStyle().
+			Background(lipgloss.Color("#bbbbbb")).
+			Foreground(lipgloss.Color("#000000")).
+			Align(lipgloss.Left).
+			Border(lipgloss.ThickBorder(), false, false, false, true).
+			BorderForeground(lipgloss.Color(conf.ColorBlur))
 )
 
 type Model struct {
+	path *utils.PathParts
 }
 
-func New() *Model {
-	m := &Model{}
+func New(path *utils.PathParts) *Model {
+	m := &Model{
+		path: path,
+	}
 	return m
 }
 
@@ -31,12 +44,17 @@ func (m *Model) Update(msg tea.Msg) (*Model, tea.Cmd) {
 	var cmds []tea.Cmd
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		barStyle = barStyle.Width(msg.Width - 2)
+		shortcutStyle = shortcutStyle.Width(msg.Width/2 - 2)
+		infoStyle = infoStyle.Width(msg.Width - shortcutStyle.GetWidth() - 2)
 	}
 	return m, tea.Batch(cmds...)
 }
 
 func (m *Model) View() string {
-	out := "Ctrl+x → execute・ESC → exit・"
-	return barStyle.Render(out)
+	out := "<Ctrl+x> → execute・<ESC> → exit・"
+	path := fmt.Sprintf("・%s/%s/%s", m.path.ConfigConnection, m.path.DatabaseName, m.path.SchemaName)
+	r := lipgloss.JoinHorizontal(lipgloss.Left,
+		infoStyle.Render(path),
+		shortcutStyle.Render(out))
+	return r
 }
